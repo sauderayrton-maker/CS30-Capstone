f4d35d8ea5da9b64395a1b5f0e83ae3abc634c3f
Lost in the Sauce <sauder.ayrton@gmail.com>
2026-06-08 22:39:46 -0600

    added grid effect from login to flashbang

--- applets/flashbang.js
+++ applets/flashbang.js
@@ -1,31 +1,100 @@
-let brightness = 255; // starts blinding white
+const BACKGROUND = "#0f0d0a";
+const TILE_DARK = "#1a1714";
+const TILE_DARKER = "#161208";
+const COLORS = [
+  "#e6c200",
+  "#d4a573",
+  "#e6d960",
+  "#d47a8f",
+  "#a8d46b",
+  "#6ad4a4",
+  "#6ac4e6",
+  "#b96ae6",
+];
+
+let brightness = 255;
+let tiles = [],
+  tileSize = 50,
+  cols,
+  rows;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont("opensans");
+  textFont("Open Sans");
+  buildGrid();
 }
 
 function draw() {
-  background(brightness);
+  background(BACKGROUND);
+  for (let t of tiles) {
+    t.update();
+    t.display();
+  }
   words();
   changeColor();
 }
 
+function buildGrid() {
+  tiles = [];
+  cols = ceil(width / tileSize);
+  rows = ceil(height / tileSize);
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      tiles.push(new Tile(x * tileSize, y * tileSize, x));
+    }
+  }
+}
+
 function changeColor() {
   if (brightness > 0) {
-    brightness -= 0.5; // slowly fade to black
-    console.log(brightness); // unnecessary but here we are
+    brightness -= 0.5;
   }
 }
 
 function words() {
-  fill(255);
-  textSize(50);
   noStroke();
   textAlign(CENTER, CENTER);
-  text(
-    "Who has ever wanted light mode. Go back to the last tab. Ew.",
-    width / 2,
-    height / 2,
-  );
+  textSize(72);
+  fill(255);
+  text("EWWWWWWW", width / 2, height / 2 - 50);
+  textSize(18);
+  fill(200);
+  text(" click to go back.", width / 2, height / 2 + 30);
+}
+
+class Tile {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+    this.baseColor =
+      floor(col / 4) % 2 === 0 ? color(TILE_DARK) : color(TILE_DARKER);
+    this.glowColor = color(random(COLORS));
+    this.charge = 0;
+  }
+
+  update() {
+    let d = dist(mouseX, mouseY, this.x + tileSize / 2, this.y + tileSize / 2);
+    if (d < 100) {
+      this.charge = map(d, 0, 100, 1, 0);
+    } else {
+      this.charge = max(0, this.charge - 0.04);
+    }
+  }
+
+  display() {
+    let dark = lerpColor(this.baseColor, this.glowColor, this.charge);
+    let fade = map(brightness, 255, 0, 0, 1);
+    noStroke();
+    fill(lerpColor(color(brightness), dark, fade));
+    rect(this.x + 2, this.y + 2, tileSize - 4, tileSize - 4, 4);
+  }
+}
+
+function mousePressed() {
+  history.back();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  buildGrid();
 }
